18c76df55c3424595b41e4a7e9d6d60e668973e6
0pportunity <w24@ro.ru>
2022-10-31 01:17:38 +0900

    добавлено задание 2

---
+++ Task 13.6.2/Ten_Often_Words/Program.cs
@@ -0,0 +1,8 @@
+﻿using Ten_Often_Words;
+
+Console.WriteLine("Введите путь до файла с текстом:");
+string path = Console.ReadLine();
+string text = File.ReadAllText(path);
+WordFrequencyCounter wfc = new(text);
+wfc.ShowTop(10);
+Console.ReadKey();
---
+++ Task 13.6.2/Ten_Often_Words/WordFrequencyCounter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ten_Often_Words
+{
+    internal class WordFrequencyCounter
+    {
+        public string text;
+
+        public WordFrequencyCounter(string text)
+        {
+            this.text = text;
+        }
+
+        private string RemovePunctuation()
+        {
+            return new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+        }
+
+        /// <summary>
+        /// Подсчёт количества слов
+        /// </summary>
+        /// <returns></returns>
+        private Dictionary<string, int> CountWords()
+        {
+            // Избавляемся от знаков пунктуации и приводим к верхнему регистру
+            string noPunctuationTextUpper = RemovePunctuation().ToUpper();
+
+            // Формируем список слов, учитывая "пустые" символы (пробел, перенос строки и т.п.)
+            char[] emptyChar = { '\u0020', '\u0000', '\u2063', '\u000A', '\u000B', '\u000C', '\u000D', '\u0085', '\u2028', '\u2029', '\u00A0', '\u00B6', '\t', '\n'};
+            List<string> allWords = noPunctuationTextUpper.Split(emptyChar).ToList();
+
+            // Производим подсчёт уникальных слов, добавляя результаты в словарь
+            var wordQuantity = new Dictionary<string, int>();
+            foreach (string word in allWords)
+            {
+                if (wordQuantity.ContainsKey(word))
+                {
+                    wordQuantity[word] = wordQuantity[word] + 1;
+                }
+                else
+                {
+                   wordQuantity.Add(word, 1);
+                }
+            }
+
+            // Выполним сортировку по значениям
+            wordQuantity = wordQuantity.OrderByDescending(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+            return wordQuantity;
+        }
+
+        /// <summary>
+        /// Формирование топ-списка
+        /// </summary>
+        /// <param name="topAmount"></param>
+        /// <returns></returns>
+        private LinkedList<KeyValuePair<string, int>> TopRepeatedWords (int topAmount)
+        {
+            var topWords = new LinkedList<KeyValuePair<string, int>>();
+            var countedWords = CountWords();
+            foreach (var word in countedWords)
+            {
+                if (topAmount <= 0)
+                {
+                    if (word.Value == topWords.Last().Value) 
+                    {
+                        // Добавление в случае, если несколько слов встречаются одинаковое
+                        // количество раз и одно из них занимает последнюю строчку в топе
+                        topWords.AddLast(word);
+                    }
+                    else { break; }
+                }
+                else { topWords.AddLast(word); topAmount--; }
+            }
+            return topWords;
+        }
+
+        public void ShowTop (int topAmount)
+        {
+            if(topAmount > 0)
+            {
+                var topList = TopRepeatedWords(topAmount);
+                Console.WriteLine($"Список {topAmount} наиболее часто встречающихся слов:");
+                int i = 0;
+                foreach (var position in topList)
+                {
+                    if (i == topAmount) { Console.WriteLine("Также одинаково часто встречаются:"); }
+                    Console.WriteLine($"\t({position.Value})\t{position.Key}");
+                    i++;
+                }
+            }
+        }
+    }
+}
